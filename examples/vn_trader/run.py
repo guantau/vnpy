698525010c82186e10a0cdc93538dcbd76e64bae
guantau <alphatau@163.com>
2019-08-25 11:16:40 +0800

    add data recorder example

--- examples/vn_trader/run.py
+++ examples/vn_trader/run.py
@@ -4,25 +4,25 @@ from vnpy.event import EventEngine
 from vnpy.trader.engine import MainEngine
 from vnpy.trader.ui import MainWindow, create_qapp
 
-# from vnpy.gateway.binance import BinanceGateway
+from vnpy.gateway.binance import BinanceGateway
 # from vnpy.gateway.bitmex import BitmexGateway
 # from vnpy.gateway.futu import FutuGateway
 # from vnpy.gateway.ib import IbGateway
 # from vnpy.gateway.ctp import CtpGateway
 # from vnpy.gateway.ctptest import CtptestGateway
 # from vnpy.gateway.mini import MiniGateway
-from vnpy.gateway.sopt import SoptGateway
+# from vnpy.gateway.sopt import SoptGateway
 # from vnpy.gateway.minitest import MinitestGateway
 # from vnpy.gateway.femas import FemasGateway
 # from vnpy.gateway.tiger import TigerGateway
 # from vnpy.gateway.oes import OesGateway
 from vnpy.gateway.okex import OkexGateway
-# from vnpy.gateway.huobi import HuobiGateway
+from vnpy.gateway.huobi import HuobiGateway
 from vnpy.gateway.bitfinex import BitfinexGateway
 # from vnpy.gateway.onetoken import OnetokenGateway
 from vnpy.gateway.okexf import OkexfGateway
 # from vnpy.gateway.xtp import XtpGateway
-# from vnpy.gateway.hbdm import HbdmGateway
+from vnpy.gateway.hbdm import HbdmGateway
 # from vnpy.gateway.tap import TapGateway
 # from vnpy.gateway.tora import ToraGateway
 # from vnpy.gateway.alpaca import AlpacaGateway
@@ -31,7 +31,7 @@ from vnpy.app.cta_strategy import CtaStrategyApp
 # from vnpy.app.csv_loader import CsvLoaderApp
 # from vnpy.app.algo_trading import AlgoTradingApp
 from vnpy.app.cta_backtester import CtaBacktesterApp
-# from vnpy.app.data_recorder import DataRecorderApp
+from vnpy.app.data_recorder import DataRecorderApp
 # from vnpy.app.risk_manager import RiskManagerApp
 from vnpy.app.script_trader import ScriptTraderApp
 from vnpy.app.rpc_service import RpcServiceApp
@@ -45,11 +45,11 @@ def main():
 
     main_engine = MainEngine(event_engine)
 
-    # main_engine.add_gateway(BinanceGateway)
+    main_engine.add_gateway(BinanceGateway)
     # main_engine.add_gateway(CtpGateway)
     # main_engine.add_gateway(CtptestGateway)
     # main_engine.add_gateway(MiniGateway)
-    main_engine.add_gateway(SoptGateway)
+    # main_engine.add_gateway(SoptGateway)
     # main_engine.add_gateway(MinitestGateway)
     # main_engine.add_gateway(FemasGateway)
     # main_engine.add_gateway(IbGateway)
@@ -57,12 +57,12 @@ def main():
     # main_engine.add_gateway(BitmexGateway)
     # main_engine.add_gateway(TigerGateway)
     # main_engine.add_gateway(OesGateway)
-    # main_engine.add_gateway(OkexGateway)
-    # main_engine.add_gateway(HuobiGateway)
+    main_engine.add_gateway(OkexGateway)
+    main_engine.add_gateway(HuobiGateway)
     # main_engine.add_gateway(BitfinexGateway)
     # main_engine.add_gateway(OnetokenGateway)
-    # main_engine.add_gateway(OkexfGateway)
-    # main_engine.add_gateway(HbdmGateway)
+    main_engine.add_gateway(OkexfGateway)
+    main_engine.add_gateway(HbdmGateway)
     # main_engine.add_gateway(XtpGateway)
     # main_engine.add_gateway(TapGateway)
     # main_engine.add_gateway(ToraGateway)
@@ -72,7 +72,7 @@ def main():
     main_engine.add_app(CtaBacktesterApp)
     # main_engine.add_app(CsvLoaderApp)
     # main_engine.add_app(AlgoTradingApp)
-    # main_engine.add_app(DataRecorderApp)
+    main_engine.add_app(DataRecorderApp)
     # main_engine.add_app(RiskManagerApp)
     main_engine.add_app(ScriptTraderApp)
     main_engine.add_app(RpcServiceApp)
